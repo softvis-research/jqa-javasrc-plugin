4235eb503072e381775f2f4d342843e3a3fcf27e
valmynd <valmynd@localhost>
2017-12-13 21:08:45 +0100

    huge refactoring (got rid of some FIXMEs and redundant code)

--- src/main/java/org/unileipzig/jqassistant/plugin/parser/impl/scanner/JavaSourceFileScannerPlugin.java
+++ src/main/java/org/unileipzig/jqassistant/plugin/parser/impl/scanner/JavaSourceFileScannerPlugin.java
@@ -12,14 +12,12 @@ import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.PackageDeclaration;
-import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.stmt.ReturnStmt;
 import com.github.javaparser.ast.stmt.Statement;
-import com.github.javaparser.ast.type.ClassOrInterfaceType;
-import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.resolution.Resolvable;
 import com.github.javaparser.resolution.declarations.*;
-import com.github.javaparser.resolution.types.ResolvedArrayType;
 import com.github.javaparser.resolution.types.ResolvedReferenceType;
 import com.github.javaparser.resolution.types.ResolvedType;
 import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserMethodDeclaration;
@@ -55,22 +53,7 @@ public class JavaSourceFileScannerPlugin extends AbstractScannerPlugin<FileResou
             String packageName = packageDeclaration.isPresent() ? packageDeclaration.get().getNameAsString() : "";
             List<TypeDescriptor> typeDescriptors = javaSourceFileDescriptor.getTypes();
             for (TypeDeclaration<?> typeDeclaration : cu.getTypes()) {
-                String fqn = packageName + "." + typeDeclaration.getName();
-                TypeDescriptor typeDescriptor = null;
-                if (typeDeclaration instanceof Resolvable) {
-                    Object resolved = ((Resolvable) typeDeclaration).resolve();
-                    if (resolved instanceof ResolvedClassDeclaration) {
-                        typeDescriptor = this.handleClass((ResolvedClassDeclaration) resolved);
-                    } else if (resolved instanceof ResolvedInterfaceDeclaration) {
-                        // TODO
-                    } else if (resolved instanceof ResolvedEnumDeclaration) {
-                        // TODO
-                    } else if (resolved instanceof ResolvedAnnotationDeclaration) {
-                        // TODO
-                    } else {
-                        System.out.println("!!! Unexpected Resolvable in ClassFile: " + typeDeclaration);
-                    }
-                }
+                TypeDescriptor typeDescriptor = this.handleType(typeDeclaration);
                 if (typeDescriptor != null) typeDescriptors.add(typeDescriptor);
                 else System.out.println("WARNING: couldn't handle type in " + cu);
             }
@@ -80,109 +63,53 @@ public class JavaSourceFileScannerPlugin extends AbstractScannerPlugin<FileResou
 
     /**
      * Create TypeDescriptor (XO) from TypeDeclaration (JavaParser)
+     * ... it needs to be (and most probably is) something that implements Resolvable
      */
-    private TypeDescriptor handleType(String fullyQualifiedName, TypeDeclaration typeDeclaration) {
-        if (typeDeclaration.isClassOrInterfaceDeclaration()) {
-            if (!resolver.has(fullyQualifiedName)) {
-                return this.handleClass(fullyQualifiedName, typeDeclaration.asClassOrInterfaceDeclaration());
+    private TypeDescriptor handleType(TypeDeclaration typeDeclaration) {
+        TypeDescriptor typeDescriptor = null;
+        if (typeDeclaration instanceof Resolvable) {
+            try {
+                Object resolved = ((Resolvable) typeDeclaration).resolve();
+                typeDescriptor = this.handleType(resolved);
+                //System.out.println("was able to resolve " + resolved);
+            } catch (RuntimeException e) { // actually is UnsolvedSymbolException
+                System.out.println("could not resolve " + typeDeclaration + " e: " + e);
+                // TODO: return some replacement/placeholder ("external dependency")
+            }
+        } else {
+            System.out.println("!!! Unexpected Type that doesn't implement Resolvable: " + typeDeclaration);
+        }
+        return typeDescriptor;
+    }
+
+    /**
+     * Create TypeDescriptor (XO) from a Resolved* Object (JavaParser)
+     * need to differentiate e.g. between JavaParserClassDeclaration and ReflectionClassDeclaration
+     */
+    private TypeDescriptor handleType(Object resolved) {
+        if (Utils.whichSolverWasUsed(resolved) == Utils.SolverType.JavaParserSolver) {
+            if (resolved instanceof ResolvedClassDeclaration) {
+                return this.handleClass((ResolvedClassDeclaration) resolved);
+            } else if (resolved instanceof ResolvedInterfaceDeclaration) {
+                // TODO
+            } else if (resolved instanceof ResolvedEnumDeclaration) {
+                // TODO
+            } else if (resolved instanceof ResolvedAnnotationDeclaration) {
+                // TODO
             } else {
-                return resolver.get(fullyQualifiedName, ClassFileDescriptor.class);
+                System.out.println("!!! Unexpected Resolvable: " + resolved);
             }
-        } else if (typeDeclaration.isEnumDeclaration()) {
-            // TODO
-            System.out.println("ENUM found");
+        } else {
+            System.out.println("TODO: Need to handle builtin or unresolvable types"
+                + ", e.g.: " + resolved
+                + " - used resolver: " + Utils.whichSolverWasUsed(resolved));
         }
+
         return null; // throw?
-    }
-
-    /**
-     * Create TypeDescriptor (XO) from Type (or rather ResolvedType) (JavaParser)
-     */
-    private TypeDescriptor handleType(Type type) {
-        //if (resolver.has(fullyQualifiedName)) { // TODO: need to construct fullyQualifiedName from some information
-        //    return resolver.get(fullyQualifiedName, TypeDescriptor.class); // more concrete validation?
-        //}
-        try {
-            ResolvedType resolvedType = type.resolve();
-            if (resolvedType.isArray()) {
-                ResolvedArrayType resolvedArrayType = resolvedType.asArrayType();
-                System.out.println("ARRAY" + resolvedArrayType);
-            } else if (resolvedType.isReferenceType()) {
-                ResolvedReferenceType resolvedReferenceType = resolvedType.asReferenceType();
-                ResolvedReferenceTypeDeclaration resolvedDeclaration = resolvedReferenceType.getTypeDeclaration();
-                if (resolvedDeclaration.isClass()) {
-                    // typecast to ResolvedClassDeclaration?
-                    // need to differentiate between JavaParserClassDeclaration and ReflectionClassDeclaration
-                    // ReflectionClassDeclaration refers to a builtin type, such as String
-                } else if (resolvedDeclaration.isEnum()) {
-                    // TODO
-                } else if (resolvedDeclaration.isInterface()) {
-                    // TODO
-                }
-            }
-        } catch (RuntimeException e) { // actually is UnsolvedSymbolException
-            System.out.println("could not resolve " + type + " e: " + e);
-            // TODO: return some replacement/placeholder ("external dependency")
-        }
-        return null; // throw?
-    }
-
-    /**
-     * Create ClassFileDescriptor (XO) from ClassOrInterfaceDeclaration (JavaParser)
-     */
-    @Deprecated
-    private ClassFileDescriptor handleClass(String fullyQualifiedName, ClassOrInterfaceDeclaration typeDeclaration) {
-        ClassFileDescriptor classFileDescriptor = resolver.create(fullyQualifiedName, ClassFileDescriptor.class);
-        List<MemberDescriptor> memberDescriptors = classFileDescriptor.getDeclaredMembers();
-        List<MethodDescriptor> methodDescriptors = classFileDescriptor.getDeclaredMethods();
-        List<FieldDescriptor> fieldDescriptors = classFileDescriptor.getDeclaredFields();
-        // get methods and fields
-        for (MethodDeclaration mD : typeDeclaration.getMethods()) {
-            String fullyQualifiedMethodName = fullyQualifiedName + "." + mD.getName();
-            System.out.println("MethodDeclaration: " + fullyQualifiedMethodName);
-            MethodDescriptor methodDescriptor = resolver.create(fullyQualifiedMethodName, MethodDescriptor.class);
-            methodDescriptor.setName(mD.getName().toString());
-            methodDescriptor.setSignature(mD.getSignature().toString());
-            memberDescriptors.add(methodDescriptor);
-            methodDescriptors.add(methodDescriptor);
-
-            int parameterIndex = 0;
-            List<ParameterDescriptor> parameterDescriptors = methodDescriptor.getParameters();
-            for (Parameter p : mD.getParameters()) {
-                ParameterDescriptor parameterDescriptor = store.create(ParameterDescriptor.class);
-                //parameterDescriptor.setType(resolver.getOrResolve(p.getType()));
-                parameterDescriptor.setType(this.handleType(p.getType()));
-                parameterDescriptor.setIndex(parameterIndex++);
-                parameterDescriptors.add(parameterDescriptor);
-            }
-            mD.getBody().ifPresent((body) -> {
-                for (Statement statement : body.getStatements()) {
-                    System.out.println("MethodBodyStatement: " + statement);
-                }
-            });
-        }
-        for (FieldDeclaration fD : typeDeclaration.getFields()) {
-            System.out.println("FieldDeclaration: " + fD);
-        }
-        // get superclasses / implemented interfaces
-        for (ClassOrInterfaceType superClassType : typeDeclaration.asClassOrInterfaceDeclaration().getExtendedTypes()) {
-            // FIXME: since it is a list, does getExtendedTypes() also include interfaces?? -> in that case we need to filter
-            // TODO: explore using ClassOrInterfaceDeclaration.resolve() at the very beginning to have only one API to take care of!
-            TypeDescriptor superClassDescriptor = this.handleType(superClassType);
-            classFileDescriptor.setSuperClass(superClassDescriptor);
-        }
-        List<TypeDescriptor> interfaces = classFileDescriptor.getInterfaces();
-        for (ClassOrInterfaceType interfaceType : typeDeclaration.getImplementedTypes()) {
-            TypeDescriptor interfaceDescriptor = this.handleType(interfaceType);
-            interfaces.add(interfaceDescriptor);
-        }
-        return classFileDescriptor;
     }
 
     /**
      * Create ClassFileDescriptor (XO) from ResolvedClassDeclaration (JavaParser)
-     * ResolvedClassDeclaration seems to be somewhat similar to ClassOrInterfaceDeclaration, but is everything but...
-     * For situations
      * TODO: need to differentiate between JavaParserClassDeclaration and ReflectionClassDeclaration
      * - e.g. for a JavaParserClassDeclaration, we WANT TO know everything really detailed
      * - e.g. for a ReflectionClassDeclaration, we DON'T, since it's a builtin class
@@ -210,13 +137,14 @@ public class JavaSourceFileScannerPlugin extends AbstractScannerPlugin<FileResou
                 ResolvedParameterDeclaration p = m.getParam(i);
                 ParameterDescriptor parameterDescriptor = store.create(ParameterDescriptor.class);
                 parameterDescriptor.setIndex(i);
-                TypeDescriptor parameterTypeDescriptor = null;//this.handleType(p.getType()); // FIXME
+                TypeDescriptor parameterTypeDescriptor = this.handleType(p.getType());
                 parameterDescriptor.setType(parameterTypeDescriptor);
                 // name seems to (currently!) not be relevant for ParameterDescriptor
                 // interesting: hasVariadicParameter ...  seems also currently not to be relevant
                 parameterDescriptors.add(parameterDescriptor);
             }
-            TypeDescriptor returnTypeDescriptor = null;//this.handleType(m.getReturnType()); // FIXME
+            // handle (specified) return type (it would also be possible to be retrieved from body statements)
+            TypeDescriptor returnTypeDescriptor = this.handleType(m.getReturnType());
             methodDescriptor.setReturns(returnTypeDescriptor);
             // handle (specified) thrown exceptions (otherwise needs to be retrieved from body statements)
             for (ResolvedType e : m.getSpecifiedExceptions()) {
@@ -250,7 +178,14 @@ public class JavaSourceFileScannerPlugin extends AbstractScannerPlugin<FileResou
         for (ResolvedFieldDeclaration fD : resolvedClass.getDeclaredFields()) {
             System.out.println("Resolved FieldDeclaration: " + fD);
         }
-        // ...
+        // get superclasses / implemented interfaces
+        TypeDescriptor superClassDescriptor = this.handleType(resolvedClass.getSuperClass());
+        classDescriptor.setSuperClass(superClassDescriptor);
+        List<TypeDescriptor> interfaces = classDescriptor.getInterfaces();
+        for (ResolvedReferenceType interfaceType : resolvedClass.getInterfaces()) {
+            TypeDescriptor interfaceDescriptor = this.handleType(interfaceType);
+            interfaces.add(interfaceDescriptor);
+        }
         return classDescriptor;
     }
 }
---
+++ src/main/java/org/unileipzig/jqassistant/plugin/parser/impl/scanner/Utils.java
@@ -0,0 +1,23 @@
+package org.unileipzig.jqassistant.plugin.parser.impl.scanner;
+
+public class Utils {
+    public enum SolverType {
+        JavaParserSolver,
+        ReflectionSolver,
+        Unknown
+    }
+
+    static public SolverType whichSolverWasUsed(Object resolved) { // ResolvedDeclaration
+        // turns out some Classes have a typeSolver reference for the solver that was used, but that is private
+        // and somewhere in the comments it's mentioned that such references shall be removed (ReferenceTypeImpl.java)
+        // thus for now some hackery until a better solution is found
+        String clsName = resolved.getClass().getSimpleName();
+        if(clsName.startsWith("JavaParser")) {
+            return SolverType.JavaParserSolver;
+        }
+        if(clsName.startsWith("Reflection")) {
+            return SolverType.ReflectionSolver;
+        }
+        return SolverType.Unknown;
+    }
+}
