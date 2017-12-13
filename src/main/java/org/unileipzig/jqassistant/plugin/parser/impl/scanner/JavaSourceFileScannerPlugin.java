376d7f48326c756c727e137e049926727a25bdbb
valmynd <valmynd@localhost>
2017-12-13 18:12:00 +0100

    more progress, next thing on the agenda: use ClassOrInterfaceDeclaration::resolve() at the very beginning to have only one API to take care of, which in some aspects better seems to have some advantages, such as clear differentiation between Interfaces and Classes, for Example (but it may end up being a failure...)

--- src/main/java/org/unileipzig/jqassistant/plugin/parser/impl/scanner/JavaSourceFileScannerPlugin.java
+++ src/main/java/org/unileipzig/jqassistant/plugin/parser/impl/scanner/JavaSourceFileScannerPlugin.java
@@ -10,17 +10,19 @@ import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractScannerPlugin
 import com.buschmais.jqassistant.plugin.common.api.scanner.filesystem.FileResource;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.PackageDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.resolution.declarations.ResolvedClassDeclaration;
 import com.github.javaparser.resolution.declarations.ResolvedFieldDeclaration;
 import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
+import com.github.javaparser.resolution.types.ResolvedArrayType;
 import com.github.javaparser.resolution.types.ResolvedReferenceType;
+import com.github.javaparser.resolution.types.ResolvedType;
 import org.unileipzig.jqassistant.plugin.parser.api.model.*;
 import org.unileipzig.jqassistant.plugin.parser.api.scanner.JavaScope;
 
@@ -49,30 +51,75 @@ public class JavaSourceFileScannerPlugin extends AbstractScannerPlugin<FileResou
         // parse files and determine concrete types (i.e. class, interface, annotation or enum)
         try (InputStream in = item.createStream()) {
             CompilationUnit cu = JavaParser.parse(in);
-            //System.out.println("CU: " + cu);
             Optional<PackageDeclaration> packageDeclaration = cu.getPackageDeclaration();
             String packageName = packageDeclaration.isPresent() ? packageDeclaration.get().getNameAsString() : "";
+            List<TypeDescriptor> typeDescriptors = javaSourceFileDescriptor.getTypes();
             for (TypeDeclaration<?> typeDeclaration : cu.getTypes()) {
                 String fqn = packageName + "." + typeDeclaration.getName();
-                if (typeDeclaration.isClassOrInterfaceDeclaration()) {
-                    if (!resolver.has(fqn)) this.handleClass(fqn, typeDeclaration.asClassOrInterfaceDeclaration());
-                } else if (typeDeclaration.isEnumDeclaration()) {
-                    // TODO
-                    System.out.println("ENUM found");
-                }
+                TypeDescriptor typeDescriptor = this.handleType(fqn, typeDeclaration);
+                typeDescriptors.add(typeDescriptor);
             }
         }
         return javaSourceFileDescriptor;
     }
 
     /**
+     * Create TypeDescriptor (XO) from TypeDeclaration (JavaParser)
+     */
+    private TypeDescriptor handleType(String fullyQualifiedName, TypeDeclaration typeDeclaration) {
+        if (typeDeclaration.isClassOrInterfaceDeclaration()) {
+            if (!resolver.has(fullyQualifiedName)) {
+                return this.handleClass(fullyQualifiedName, typeDeclaration.asClassOrInterfaceDeclaration());
+            } else {
+                return resolver.get(fullyQualifiedName, ClassFileDescriptor.class);
+            }
+        } else if (typeDeclaration.isEnumDeclaration()) {
+            // TODO
+            System.out.println("ENUM found");
+        }
+        return null; // throw?
+    }
+
+    /**
+     * Create TypeDescriptor (XO) from Type (or rather ResolvedType) (JavaParser)
+     */
+    private TypeDescriptor handleType(Type type) {
+        //if (resolver.has(fullyQualifiedName)) { // TODO: need to construct fullyQualifiedName from some information
+        //    return resolver.get(fullyQualifiedName, TypeDescriptor.class); // more concrete validation?
+        //}
+        try {
+            ResolvedType resolvedType = type.resolve();
+            if (resolvedType.isArray()) {
+                ResolvedArrayType resolvedArrayType = resolvedType.asArrayType();
+                System.out.println("ARRAY" + resolvedArrayType);
+            } else if (resolvedType.isReferenceType()) {
+                ResolvedReferenceType resolvedReferenceType = resolvedType.asReferenceType();
+                ResolvedReferenceTypeDeclaration resolvedDeclaration = resolvedReferenceType.getTypeDeclaration();
+                if (resolvedDeclaration.isClass()) {
+                    // typecast to ResolvedClassDeclaration?
+                    // need to differentiate between JavaParserClassDeclaration and ReflectionClassDeclaration
+                    // ReflectionClassDeclaration refers to a builtin type, such as String
+                } else if (resolvedDeclaration.isEnum()) {
+                    // TODO
+                } else if (resolvedDeclaration.isInterface()) {
+                    // TODO
+                }
+            }
+        } catch (RuntimeException e) { // actually is UnsolvedSymbolException
+            System.out.println("could not resolve " + type + " e: " + e);
+            // TODO: return some replacement/placeholder ("external dependency")
+        }
+        return null; // throw?
+    }
+
+    /**
      * Create ClassFileDescriptor (XO) from ClassOrInterfaceDeclaration (JavaParser)
      */
-    private void handleClass(String fullyQualifiedName, ClassOrInterfaceDeclaration typeDeclaration) {
+    private ClassFileDescriptor handleClass(String fullyQualifiedName, ClassOrInterfaceDeclaration typeDeclaration) {
         ClassFileDescriptor classFileDescriptor = resolver.create(fullyQualifiedName, ClassFileDescriptor.class);
-        List<MemberDescriptor> members = classFileDescriptor.getDeclaredMembers();
-        List<MethodDescriptor> methods = classFileDescriptor.getDeclaredMethods();
-        List<FieldDescriptor> fields = classFileDescriptor.getDeclaredFields();
+        List<MemberDescriptor> memberDescriptors = classFileDescriptor.getDeclaredMembers();
+        List<MethodDescriptor> methodDescriptors = classFileDescriptor.getDeclaredMethods();
+        List<FieldDescriptor> fieldDescriptors = classFileDescriptor.getDeclaredFields();
         // get methods and fields
         for (MethodDeclaration mD : typeDeclaration.getMethods()) {
             String fullyQualifiedMethodName = fullyQualifiedName + "." + mD.getName();
@@ -80,8 +127,39 @@ public class JavaSourceFileScannerPlugin extends AbstractScannerPlugin<FileResou
             MethodDescriptor methodDescriptor = resolver.create(fullyQualifiedMethodName, MethodDescriptor.class);
             methodDescriptor.setName(mD.getName().toString());
             methodDescriptor.setSignature(mD.getSignature().toString());
-            members.add(methodDescriptor);
-            methods.add(methodDescriptor);
+            memberDescriptors.add(methodDescriptor);
+            methodDescriptors.add(methodDescriptor);
+            for (Modifier m : mD.getModifiers()) {
+                switch (m) { // TODO: STRICTFP, VOLATILE, etc. pp.
+                    case PUBLIC:
+                    case PRIVATE:
+                    case PROTECTED:
+                    case DEFAULT:
+                        methodDescriptor.setVisibility(m.toString());
+                        break;
+                    case FINAL:
+                        methodDescriptor.setFinal(true);
+                        break;
+                    case NATIVE:
+                        methodDescriptor.setNative(true);
+                        break;
+                    case STATIC:
+                        methodDescriptor.setStatic(true);
+                        break;
+                    case ABSTRACT:
+                        methodDescriptor.setAbstract(true);
+                        break;
+                }
+            }
+            int parameterIndex = 0;
+            List<ParameterDescriptor> parameterDescriptors = methodDescriptor.getParameters();
+            for (Parameter p : mD.getParameters()) {
+                ParameterDescriptor parameterDescriptor = store.create(ParameterDescriptor.class);
+                //parameterDescriptor.setType(resolver.getOrResolve(p.getType()));
+                parameterDescriptor.setType(this.handleType(p.getType()));
+                parameterDescriptor.setIndex(parameterIndex++);
+                parameterDescriptors.add(parameterDescriptor);
+            }
             mD.getBody().ifPresent((body) -> {
                 for (Statement statement : body.getStatements()) {
                     System.out.println("MethodBodyStatement: " + statement);
@@ -93,25 +171,24 @@ public class JavaSourceFileScannerPlugin extends AbstractScannerPlugin<FileResou
         }
         // get superclasses / implemented interfaces
         for (ClassOrInterfaceType superClassType : typeDeclaration.asClassOrInterfaceDeclaration().getExtendedTypes()) {
-            try {
-                ResolvedReferenceType x = superClassType.resolve();
-                //javaParserFacade.solve(ClassOrInterfaceDeclaration.class);
-                //ResolvedType y = javaParserFacade.solve(superClassType);
-                System.out.println("Superclass: " + superClassType + " resolved " + x.getTypeDeclaration());
-            } catch (RuntimeException e) { // actually is UnsolvedSymbolException
-                System.out.println("could not resolve " + superClassType.getName() + " e: " + e);
-            }
-            //String fqn = superClassType.();
-            //TypeDescriptor superClassDescriptor = resolver.resolveType("com.acme.MySuperClass", context);
-            //classFileDescriptor.setExtends(superClassDescriptor);
+            // FIXME: since it is a list, does getExtendedTypes() also include interfaces?? -> in that case we need to filter
+            // TODO: explore using ClassOrInterfaceDeclaration.resolve() at the very beginning to have only one API to take care of!
+            TypeDescriptor superClassDescriptor = this.handleType(superClassType);
+            classFileDescriptor.setSuperClass(superClassDescriptor);
         }
+        List<TypeDescriptor> interfaces = classFileDescriptor.getInterfaces();
+        for (ClassOrInterfaceType interfaceType : typeDeclaration.getImplementedTypes()) {
+            TypeDescriptor interfaceDescriptor = this.handleType(interfaceType);
+            interfaces.add(interfaceDescriptor);
+        }
+        return classFileDescriptor;
     }
 
     /**
      * Create ClassFileDescriptor (XO) from ResolvedClassDeclaration (JavaParser)
      * ResolvedClassDeclaration seems to be somewhat similar to ClassOrInterfaceDeclaration, but is everything but...
      */
-    private void handleClass(String fullyQualifiedName, ResolvedClassDeclaration typeDeclaration) {
+    private ClassFileDescriptor handleClass(String fullyQualifiedName, ResolvedClassDeclaration typeDeclaration) {
         ClassFileDescriptor classFileDescriptor = resolver.create(fullyQualifiedName, ClassFileDescriptor.class);
         // get methods and fields
         for (ResolvedMethodDeclaration mD : typeDeclaration.getDeclaredMethods()) {
@@ -121,5 +198,6 @@ public class JavaSourceFileScannerPlugin extends AbstractScannerPlugin<FileResou
             System.out.println("Resolved FieldDeclaration: " + fD);
         }
         // ...
+        return classFileDescriptor;
     }
 }
