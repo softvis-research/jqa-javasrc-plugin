179c09d24540f4565628e9b9964a7beb7d43acaa
Richard Müller <rmueller@wifa.uni-leipzig.de>
2018-01-19 16:37:28 +0100

    start refactoring

--- src/main/java/org/unileipzig/jqassistant/plugin/parser/impl/scanner/JavaSourceDirectoryScannerPlugin.java
+++ src/main/java/org/unileipzig/jqassistant/plugin/parser/impl/scanner/JavaSourceDirectoryScannerPlugin.java
@@ -1,13 +1,14 @@
 package org.unileipzig.jqassistant.plugin.parser.impl.scanner;
 
-import com.buschmais.jqassistant.core.scanner.api.ScannerContext;
-import com.buschmais.jqassistant.core.scanner.api.Scope;
-import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractDirectoryScannerPlugin;
+import java.io.File;
+import java.io.IOException;
+
 import org.unileipzig.jqassistant.plugin.parser.api.model.JavaSourceDirectoryDescriptor;
 import org.unileipzig.jqassistant.plugin.parser.api.scanner.JavaScope;
 
-import java.io.File;
-import java.io.IOException;
+import com.buschmais.jqassistant.core.scanner.api.ScannerContext;
+import com.buschmais.jqassistant.core.scanner.api.Scope;
+import com.buschmais.jqassistant.plugin.common.api.scanner.AbstractDirectoryScannerPlugin;
 
 public class JavaSourceDirectoryScannerPlugin extends AbstractDirectoryScannerPlugin<JavaSourceDirectoryDescriptor> {
     //private static Resolver resolver = null;
@@ -24,18 +25,15 @@ public class JavaSourceDirectoryScannerPlugin extends AbstractDirectoryScannerPl
 
     @Override
     protected void enterContainer(File container, JavaSourceDirectoryDescriptor containerDescriptor, ScannerContext scannerContext) throws IOException {
-        //do sth. when entering a directory
-        //-> cannot override scan() cause it's marked as *final*, and when the constructor is called we don't have the path information, so it seems it has to be done here of all places...
-        //if (JavaSourceDirectoryScannerPlugin.resolver == null) resolver = new Resolver(container.getPath(), scannerContext.getStore());
-        // instead of global/static access, put it into scannerContext -> make sure this is done only once as constructor of Resolver will recursively create JavaParserTypeSolver instances!
-        if (scannerContext.peekOrDefault(Resolver.class, null) == null) {
-            String path = container.getParentFile().getParentFile().getPath(); // for the HelloWorld Files, container.getPath() was not enough (!)
-            scannerContext.push(Resolver.class, new Resolver(path, scannerContext.getStore()));
+       if (scannerContext.peekOrDefault(TypeResolver.class, null) == null) {
+           //TODO find a solution for javasymbolsolver issue: https://github.com/javaparser/javasymbolsolver/issues/353	   
+    	   final String path = "src/test/java";
+           scannerContext.push(TypeResolver.class, new TypeResolver(path, scannerContext));
         }
     }
 
     @Override
     protected void leaveContainer(File container, JavaSourceDirectoryDescriptor containerDescriptor, ScannerContext scannerContext) throws IOException {
-        // do sth. when leaving a directory
+       scannerContext.pop(TypeResolver.class);
     }
 }
---
+++ src/main/java/org/unileipzig/jqassistant/plugin/parser/impl/scanner/TypeResolver.java
@@ -0,0 +1,296 @@
+package org.unileipzig.jqassistant.plugin.parser.impl.scanner;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.unileipzig.jqassistant.plugin.parser.api.model.ClassTypeDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.ConstructorDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.EnumTypeDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.FieldDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.InterfaceTypeDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.JavaSourceFileDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.MethodDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.ParameterDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.TypeDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.api.model.TypedDescriptor;
+import org.unileipzig.jqassistant.plugin.parser.impl.scanner.TypeCache.CachedType;
+
+import com.buschmais.jqassistant.core.scanner.api.ScannerContext;
+import com.buschmais.jqassistant.plugin.common.api.model.ValueDescriptor;
+import com.buschmais.jqassistant.plugin.common.api.scanner.FileResolver;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.AnnotationDeclaration;
+import com.github.javaparser.ast.body.AnnotationMemberDeclaration;
+import com.github.javaparser.ast.body.CallableDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.EnumConstantDeclaration;
+import com.github.javaparser.ast.body.EnumDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.ThisExpr;
+import com.github.javaparser.ast.stmt.ExplicitConstructorInvocationStmt;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.resolution.UnsolvedSymbolException;
+import com.github.javaparser.resolution.declarations.ResolvedAnnotationDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedAnnotationMemberDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedClassDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedConstructorDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedEnumConstantDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedEnumDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedFieldDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedMethodLikeDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedParameterDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedTypeDeclaration;
+import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
+import com.github.javaparser.resolution.types.ResolvedType;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.javaparser.Navigator;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFactory;
+import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserAnnotationMemberDeclaration;
+import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserConstructorDeclaration;
+import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserEnumConstantDeclaration;
+import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserFieldDeclaration;
+import com.github.javaparser.symbolsolver.javaparsermodel.declarations.JavaParserMethodDeclaration;
+import com.github.javaparser.symbolsolver.model.resolution.SymbolReference;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+
+/**
+ * @author Richard Müller
+ *
+ */
+public class TypeResolver {
+	private TypeSolver javaTypeSolver;
+	private Map<String, TypeCache.CachedType<? extends TypeDescriptor>> containedTypes = new HashMap<>();
+	private Map<String, TypeCache.CachedType<? extends TypeDescriptor>> requiredTypes = new HashMap<>();
+	private static final String CONSTRUCTOR_METHOD = "void <init>";
+    private ScannerContext scannerContext;
+
+ 
+	public TypeResolver(String srcDir, ScannerContext scannerContext) {
+		this.javaTypeSolver = new CombinedTypeSolver(new ReflectionTypeSolver(),
+				new JavaParserTypeSolver(new File(srcDir)));
+		JavaParser.setStaticConfiguration(new ParserConfiguration().setSymbolResolver(new JavaSymbolSolver(javaTypeSolver)));
+		this.containedTypes = new HashMap<>();
+		this.requiredTypes = new HashMap<>();
+		this.scannerContext = scannerContext;
+	}
+
+	public <T extends TypeDescriptor> TypeCache.CachedType<T> createType(String fqn, JavaSourceFileDescriptor javaSourcefileDescriptor, Class<T> type) {
+		TypeDescriptor resolvedTypeDescriptor = javaSourcefileDescriptor.resolveType(fqn);
+		T typeDescriptor = scannerContext.getStore().addDescriptorType(resolvedTypeDescriptor, type);
+        TypeCache.CachedType cachedType = new CachedType<T>(typeDescriptor);
+        containedTypes.put(fqn, cachedType);
+		requiredTypes.remove(fqn);
+		return cachedType;
+	}
+
+	public TypeCache.CachedType resolveType(String fqn) {
+		TypeCache.CachedType cachedType = containedTypes.get(fqn);
+		if (cachedType == null) {
+			String fileName = "/" + fqn.replace('.', '/') + ".java"; // Inner classes?
+			FileResolver fileResolver = scannerContext.peek(FileResolver.class);
+			JavaSourceFileDescriptor sourceFileDescriptor = fileResolver.require(fileName,
+					JavaSourceFileDescriptor.class, scannerContext);
+			cachedType = new CachedType<TypeDescriptor>(sourceFileDescriptor.resolveType(fqn));
+			requiredTypes.put(fqn, cachedType);
+		}
+		return cachedType;
+	}
+	
+	
+	
+	public MethodDescriptor getMethodDescriptor(String parentFQN, String signature) {
+		TypeCache.CachedType cachedType = resolveType(parentFQN);
+		MethodDescriptor methodDescriptor = cachedType.getMethod(signature);
+		if (methodDescriptor == null) {
+			if (signature.startsWith(CONSTRUCTOR_METHOD)) {
+				methodDescriptor = scannerContext.getStore().create(ConstructorDescriptor.class);
+			} else {
+				methodDescriptor = scannerContext.getStore().create(MethodDescriptor.class);
+			}
+			methodDescriptor.setSignature(signature);
+			cachedType.addMember(signature, methodDescriptor);
+		}
+		return methodDescriptor;
+	}
+	
+	public TypeDescriptor getTypeDescriptor(ResolvedType resolvedType) {
+		if (resolvedType.isVoid()) {
+			return resolveType("void").getTypeDescriptor();
+		} else if(resolvedType.isPrimitive()) {
+			return resolveType(resolvedType.asPrimitive().describe()).getTypeDescriptor();
+		}else if (resolvedType.isReferenceType()) {
+			return resolveType(resolvedType.asReferenceType().getQualifiedName()).getTypeDescriptor();
+		}else {
+			throw new RuntimeException("Type could not be resolved: " + resolvedType.toString());
+		}
+	}
+	
+    public ParameterDescriptor addParameterDescriptor(MethodDescriptor methodDescriptor, int index) {
+        ParameterDescriptor parameterDescriptor = scannerContext.getStore().create(ParameterDescriptor.class);
+        parameterDescriptor.setIndex(index);
+        methodDescriptor.getParameters().add(parameterDescriptor);
+        return parameterDescriptor;
+    }
+
+    public FieldDescriptor getFieldDescriptor(String parentFQN, String signature) {
+    	TypeCache.CachedType cachedType = resolveType(parentFQN);
+ 		FieldDescriptor fieldDescriptor = cachedType.getField(signature);
+        if (fieldDescriptor == null) {
+            fieldDescriptor = scannerContext.getStore().create(FieldDescriptor.class);
+            fieldDescriptor.setSignature(signature);
+            cachedType.addMember(signature, fieldDescriptor);
+        }
+        return fieldDescriptor;
+    }
+    
+    public <T extends ValueDescriptor<?>> T getValueDescriptor(Class<T> valueDescriptorType) {
+        return scannerContext.getStore().create(valueDescriptorType);
+    }
+    
+	public <T> T solveDeclaration(Node node, Class<T> resultClass) {
+		if (node instanceof MethodDeclaration) {
+            return resultClass.cast(new JavaParserMethodDeclaration((MethodDeclaration)node, javaTypeSolver));
+        }
+        if (node instanceof ClassOrInterfaceDeclaration) {
+            ResolvedReferenceTypeDeclaration resolved = JavaParserFactory.toTypeDeclaration(node, javaTypeSolver);
+            if (resultClass.isInstance(resolved)) {
+                return resultClass.cast(resolved);
+            }
+        }
+        if (node instanceof EnumDeclaration) {
+            ResolvedReferenceTypeDeclaration resolved = JavaParserFactory.toTypeDeclaration(node, javaTypeSolver);
+            if (resultClass.isInstance(resolved)) {
+                return resultClass.cast(resolved);
+            }
+        }
+        if (node instanceof EnumConstantDeclaration) {
+            ResolvedEnumDeclaration enumDeclaration = Navigator.findAncestor(node, EnumDeclaration.class).get().resolve().asEnum();
+            ResolvedEnumConstantDeclaration resolved = enumDeclaration.getEnumConstants().stream().filter(c -> ((JavaParserEnumConstantDeclaration)c).getWrappedNode() == node).findFirst().get();
+            if (resultClass.isInstance(resolved)) {
+                return resultClass.cast(resolved);
+            }
+        }
+        if (node instanceof ConstructorDeclaration) {
+            ConstructorDeclaration constructorDeclaration = (ConstructorDeclaration)node;
+            Node classOrInterfaceDeclaration = (ClassOrInterfaceDeclaration) node.getParentNode().get();
+            ResolvedClassDeclaration resolvedClass = solveDeclaration(classOrInterfaceDeclaration, ResolvedClassDeclaration.class).asClass();
+            ResolvedConstructorDeclaration resolved =  resolvedClass.getConstructors().stream().filter(c -> ((JavaParserConstructorDeclaration)c).getWrappedNode() == constructorDeclaration).findFirst().get();
+            if (resultClass.isInstance(resolved)) {
+                return resultClass.cast(resolved);
+            } 
+        }
+        if (node instanceof AnnotationDeclaration) {
+            ResolvedReferenceTypeDeclaration resolved = JavaParserFactory.toTypeDeclaration(node, javaTypeSolver);
+            if (resultClass.isInstance(resolved)) {
+                return resultClass.cast(resolved);
+            }
+        }
+        if (node instanceof AnnotationMemberDeclaration) {
+            ResolvedAnnotationDeclaration annotationDeclaration = Navigator.findAncestor(node, AnnotationDeclaration.class).get().resolve();
+            ResolvedAnnotationMemberDeclaration resolved = annotationDeclaration.getAnnotationMembers().stream().filter(c -> ((JavaParserAnnotationMemberDeclaration)c).getWrappedNode() == node).findFirst().get();
+            if (resultClass.isInstance(resolved)) {
+                return resultClass.cast(resolved);
+            }
+        }
+        if (node instanceof FieldDeclaration) {
+            FieldDeclaration fieldDeclaration = (FieldDeclaration)node;
+            if (fieldDeclaration.getVariables().size() != 1) {
+                throw new RuntimeException("Cannot resolve a Field Declaration including multiple variable declarators. Resolve the single variable declarators");
+            }
+            ResolvedFieldDeclaration resolved = new JavaParserFieldDeclaration(fieldDeclaration.getVariable(0), javaTypeSolver);
+            if (resultClass.isInstance(resolved)) {
+                return resultClass.cast(resolved);
+            }
+        }
+        if (node instanceof VariableDeclarator) {
+            ResolvedFieldDeclaration resolved = new JavaParserFieldDeclaration((VariableDeclarator)node, javaTypeSolver);
+            if (resultClass.isInstance(resolved)) {
+                return resultClass.cast(resolved);
+            }
+        }
+        if (node instanceof MethodCallExpr) {
+            SymbolReference<ResolvedMethodDeclaration> result = JavaParserFacade.get(javaTypeSolver).solve((MethodCallExpr)node);
+            if (result.isSolved()) {
+                if (resultClass.isInstance(result.getCorrespondingDeclaration())) {
+                    return resultClass.cast(result.getCorrespondingDeclaration());
+                }
+            } else {
+                throw new UnsolvedSymbolException("We are unable to find the method declaration corresponding to " + node);
+            }
+        }
+        if (node instanceof NameExpr) {
+            SymbolReference<? extends ResolvedValueDeclaration> result = JavaParserFacade.get(javaTypeSolver).solve((NameExpr) node);
+            if (result.isSolved()) {
+                if (resultClass.isInstance(result.getCorrespondingDeclaration())) {
+                    return resultClass.cast(result.getCorrespondingDeclaration());
+                }
+            } else {
+                throw new UnsolvedSymbolException("We are unable to find the value declaration corresponding to " + node);
+            }
+        }
+        if (node instanceof ThisExpr) {
+            SymbolReference<ResolvedTypeDeclaration> result = JavaParserFacade.get(javaTypeSolver).solve((ThisExpr) node);
+            if (result.isSolved()) {
+                if (resultClass.isInstance(result.getCorrespondingDeclaration())) {
+                    return resultClass.cast(result.getCorrespondingDeclaration());
+                }
+            } else {
+                throw new UnsolvedSymbolException("We are unable to find the type declaration corresponding to " + node);
+            }
+        }
+        if (node instanceof ExplicitConstructorInvocationStmt) {
+            SymbolReference<ResolvedConstructorDeclaration> result = JavaParserFacade.get(javaTypeSolver).solve((ExplicitConstructorInvocationStmt) node);
+            if (result.isSolved()) {
+                if (resultClass.isInstance(result.getCorrespondingDeclaration())) {
+                    return resultClass.cast(result.getCorrespondingDeclaration());
+                }
+            } else {
+                throw new UnsolvedSymbolException("We are unable to find the constructor declaration corresponding to " + node);
+            }
+        }
+        if (node instanceof Parameter) {
+            if (ResolvedParameterDeclaration.class.equals(resultClass)) {
+                Parameter parameter = (Parameter)node;
+                CallableDeclaration callableDeclaration = Navigator.findAncestor(node, CallableDeclaration.class).get();
+                ResolvedMethodLikeDeclaration resolvedMethodLikeDeclaration;
+                if (callableDeclaration.isConstructorDeclaration()) {
+                    resolvedMethodLikeDeclaration = callableDeclaration.asConstructorDeclaration().resolve();
+                } else {
+                    resolvedMethodLikeDeclaration = callableDeclaration.asMethodDeclaration().resolve();
+                }
+                for (int i=0;i<resolvedMethodLikeDeclaration.getNumberOfParams();i++) {
+                    if (resolvedMethodLikeDeclaration.getParam(i).getName().equals(parameter.getNameAsString())) {
+                        return resultClass.cast(resolvedMethodLikeDeclaration.getParam(i));
+                    }
+                }
+            }
+        }
+        throw new UnsupportedOperationException("Unable to find the declaration of type " + resultClass.getSimpleName()
+                + " from " + node.getClass().getSimpleName());
+}
+
+	public ResolvedTypeDeclaration solveType(ClassOrInterfaceType classOrInterfaceType) {
+		SymbolReference<ResolvedTypeDeclaration> typeDeclarationSymbolReference = JavaParserFactory
+				.getContext(classOrInterfaceType, javaTypeSolver)
+				.solveType(classOrInterfaceType.getName().getId(), javaTypeSolver);
+		if (!typeDeclarationSymbolReference.isSolved()) {
+			throw new UnsupportedOperationException();
+		}
+		return typeDeclarationSymbolReference.getCorrespondingDeclaration();
+	}
+}
