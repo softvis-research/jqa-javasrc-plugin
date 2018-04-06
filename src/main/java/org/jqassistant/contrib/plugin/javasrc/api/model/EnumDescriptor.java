fe203b1d6146c26983b58b7226f346bcc4d34e35
Richard Müller <rmueller@wifa.uni-leipzig.de>
2018-04-06 17:25:18 +0200

    set EnumDescriptor abstract

--- src/main/java/org/jqassistant/contrib/plugin/javasrc/api/model/EnumDescriptor.java
+++ src/main/java/org/jqassistant/contrib/plugin/javasrc/api/model/EnumDescriptor.java
@@ -1,7 +1,9 @@
 package org.jqassistant.contrib.plugin.javasrc.api.model;
 
+import com.buschmais.xo.api.annotation.Abstract;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 @Label("Enum")
+@Abstract
 public interface EnumDescriptor extends JavaSourceCodeDescriptor {
 }
