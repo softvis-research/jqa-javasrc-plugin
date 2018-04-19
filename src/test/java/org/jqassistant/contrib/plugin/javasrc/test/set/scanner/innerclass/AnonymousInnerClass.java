77273395f49603a7d40f95d423d365f19428a8cc
Richard <Richard@UB-RMUELLER.fritz.box>
2018-04-19 19:00:28 +0200

    add a second anonymous inner class

--- src/test/java/org/jqassistant/contrib/plugin/javasrc/test/set/scanner/innerclass/AnonymousInnerClass.java
+++ src/test/java/org/jqassistant/contrib/plugin/javasrc/test/set/scanner/innerclass/AnonymousInnerClass.java
@@ -8,7 +8,27 @@ import java.util.Iterator;
  */
 public class AnonymousInnerClass<X> {
 
-    public Iterator<X> iterator() {
+    public Iterator<X> iterator1() {
+        return new Iterator<X>() {
+            int i;
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public X next() {
+                return null;
+            }
+
+            @Override
+            public void remove() {
+            }
+        };
+    }
+
+    public Iterator<X> iterator2() {
         return new Iterator<X>() {
             int i;
 
